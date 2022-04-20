99ae7c2a763478ce1d17eedfd5977afc05c59fc8
Santiago <santiagocastanonarvizu@gmail.com>
2022-04-20 12:03:04 -0500

    initial commit

---
+++ src/test/java/com/mesaj/app/stepdefinitions/SignUpStepDefs.java
@@ -0,0 +1,19 @@
+package com.mesaj.app.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class SignUpStepDefs {
+    @Given("Pepito wants to have an account")
+    public void pepito_wants_to_have_an_account() {
+    }
+
+    @When("he sends required information to get the account")
+    public void he_sends_required_information_to_get_the_account() {
+    }
+
+    @Then("he should be told that the account was created")
+    public void he_should_be_told_that_the_account_was_created() {
+    }
+}
